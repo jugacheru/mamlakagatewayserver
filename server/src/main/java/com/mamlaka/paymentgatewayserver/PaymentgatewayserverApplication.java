52d5686804add1a7b05efe45f78f4f5699f7d61b
Julius <>
2024-09-12 08:38:39 -0700

    Added the docker configurations

--- server/src/main/java/com/mamlaka/paymentgatewayserver/PaymentgatewayserverApplication.java
+++ server/src/main/java/com/mamlaka/paymentgatewayserver/PaymentgatewayserverApplication.java
@@ -1,13 +1,17 @@
 package com.mamlaka.paymentgatewayserver;
 
+/**
+ *
+ * @author Julius
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaymentgatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentgatewayserverApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentgatewayserverApplication.class, args);
+    }
 }
